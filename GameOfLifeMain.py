6058be4bb1d40e745429c00e7ccb5b8ab0c1800e
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2023-10-17 10:17:20 +0700

    Update GameOfLifeMain.py

--- GameOfLifeMain.py
+++ GameOfLifeMain.py
@@ -7,18 +7,20 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.bgcolor("white")
+
 centre = turtle.Turtle()
 centre.shape("square")
 centre.color("green")
 centre.penup()
 centre.goto(0, 0)
+
 cell = Cell()
-i = 99
-while i > 0:
-    time.sleep(1)
+
+while True:
+    time.sleep(0.1)
     screen.update()
     cell.update_grid()
-    i -= 1
+
 screen.exitonclick()
 
 
